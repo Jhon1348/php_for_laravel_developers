d34605fc728ad230eeaf02e1038bef3fa9bca4a2
Jhon <jmoreno1@iesebre.com>
2021-10-14 19:33:02 +0200

    Primer codi PHP I HTML

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<!--separtion of concerns -> separar "coses diferents"/-->
+<!--concerns en diferents fitxers o seccions ->components-->
+<!--MVC -> Model Vista Controlador-->
+<?php
+echo 'Hola mon1!';
+?>
+</body>
+</html>
+
